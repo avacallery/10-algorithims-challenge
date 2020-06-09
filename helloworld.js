1cb53e6b75f3f17f54945136a7308ca00e543af2
avacallery <avacallery@gmail.com>
2020-06-09 14:12:40 -0400

    HelloWorld function

---
+++ helloworld.js
@@ -0,0 +1,15 @@
+/**
+ *   A line of code that prints "Hello, World!" on a new line is provided in the editor.
+ *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+ *
+ *	Parameter:
+ *   parameterVariable - A string of text.
+ **/
+
+function greeting(parameterVariable) {
+  // Write a line of code that prints parameterVariable to stdout using console.log:
+  //   const parameterVariable = 'Hello, World!';
+  console.log(parameterVariable);
+}
+
+greeting('Hello, World!');
