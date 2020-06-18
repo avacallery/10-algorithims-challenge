76264dc52d9befee2fcf0694a2ff34b0e9b5b265
avacallery <avacallery@gmail.com>
2020-06-18 14:50:42 -0400

    updated candles algorithm

--- candles.js
+++ candles.js
@@ -1,13 +1,20 @@
 //create a function that returns how many candles can be blown out in an array if only the tallest ones can be blown out
-
-function birthdayCakeCandles(age, candlesHeight) {
+function birthdayCakeCandles(candles) {
   let totalCandles = 0;
-  for (i = 0; i < candlesHeight.length; i++) {
-    if (age >= candlesHeight[i] && age <= candlesHeight[i]) {
-      totalCandles += 1;
-    }
-  }
+  // remove duplicates
+  let newCandles = [...new Set(candles)];
+
+  // get tallest candle
+  let tallestCandle = newCandles.reduce(function (a, b) {
+    return Math.max(a, b);
+  });
+
+  // count tallest candles
+  totalCandles = candles.filter(function (value) {
+    return value === tallestCandle;
+  }).length;
   console.log(totalCandles);
 }
 
-birthdayCakeCandles(3, [1, 2, 3, 3]);
+let candles = [2, 3, 4, 5, 5];
+birthdayCakeCandles(candles);
