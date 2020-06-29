4d9c988c339ed25f184497cc7b1f097565fb07e4
avacallery <avacallery@gmail.com>
2020-06-28 20:43:00 -0400

    return letter based on switch statements

---
+++ alphabet.js
@@ -0,0 +1,26 @@
+// The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+
+function getLetter(s) {
+  let letter;
+
+  switch (true) {
+    case 'aeiou'.includes(s[0]):
+      letter = 'A';
+      break;
+    case 'bcdfg'.includes(s[0]):
+      letter = 'B';
+      break;
+    case 'hjklm'.includes(s[0]):
+      letter = 'C';
+      break;
+    case 'npqrstvwxyz'.includes(s[0]):
+      letter = 'D';
+      break;
+    default:
+      text = 'Letter not found';
+  }
+
+  return letter;
+}
+
+console.log(getLetter('a'));
