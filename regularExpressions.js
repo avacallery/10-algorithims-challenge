af344f6361c01bca69ff88ba156a376dc644ee9d
avacallery <avacallery@gmail.com>
2020-07-03 12:29:11 -0400

    regular expressions I

---
+++ regularExpressions.js
@@ -0,0 +1,22 @@
+// Complete the function by returning a RegExp object, re, that matches any string s that begins and ends with the same vowel.
+
+// ^ matches beginning of input
+// $ matches end of input
+// (.) captures any character.
+// \1 is a backreference to the first capture group in our expression (i.e., (.)). It stipulates that the character following whatever we matched in step 2 must match whatever was captured inside the parentheses in step 1.
+
+function regexVar() {
+  /*
+   * Declare a RegExp object variable named 're'
+   * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+   */
+
+  const re = new RegExp(/^([aeiou]).+\1$/);
+  /*
+   * Do not remove the return statement
+   */
+  console.log(re.test('aeioua'));
+  return re;
+}
+
+regexVar('aeioua');
