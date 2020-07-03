c5c9c7e3cdba0787a078055d483227206d38fd2d
avacallery <avacallery@gmail.com>
2020-07-02 21:57:22 -0400

    get day of the week based on string

--- date.js
+++ date.js
@@ -1,7 +1,7 @@
 // Given a date string, dateString, in the format MM/DD/YYYY, find and return the day name for that date. Each day name must be one of the following strings: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, or Saturday. For example, the day name for the date 12/07/2016 is Wednesday.
 
 function getDayName(dateString) {
-  let dayNames = [
+  let dayOfTheWeek = [
     'Sunday',
     'Monday',
     'Tuesday',
@@ -11,7 +11,7 @@ function getDayName(dateString) {
     'Saturday',
   ];
 
-  return dayNames[new Date(dateString).getDay()];
+  return dayOfTheWeek[new Date(dateString).getDay()];
 }
 
-console.log(getDayName(10 / 11 / 2009, 11 / 10 / 2010));
+console.log(getDayName(10 / 11 / 2009));
