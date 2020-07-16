137f5bb4242d5fec19b918d32d65d0fe95a5196b
avacallery <avacallery@gmail.com>
2020-07-16 11:18:35 -0400

    staircase solution

--- staircase.js
+++ staircase.js
@@ -1,6 +1,6 @@
 // create staircase
 // base and height must be the same ( n ) and made up of (#'s)
-// for each denoting level, each line denotes 1 less (#) and one space
+// for each denoting level, each line denotes 1 less (#) and adds one space
 
 function staircase(n) {
   for (let i = 1; i <= n; i++) {
@@ -8,4 +8,4 @@ function staircase(n) {
   }
 }
 
-staircase(4);
+staircase(6);
