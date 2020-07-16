94432a90e564b27434346e5610ea8a79c6a3e0f6
avacallery <avacallery@gmail.com>
2020-07-16 12:52:29 -0400

    update ternary

--- angryProfessor.js
+++ angryProfessor.js
@@ -9,7 +9,7 @@ function angryProfessor(minStudents, arrivalTimes) {
   //   }
   //     return 'YES';
 
-  return arrivalTimes.length >= minStudents ? 'NO' : 'YES';
+  return arrivalTimes.length < minStudents ? 'YES' : 'NO';
 }
 
 console.log(angryProfessor(3, [-1, 3, 2, 0, 0]));
--- staircase.js
+++ staircase.js
@@ -2,6 +2,8 @@
 // base and height must be the same ( n ) and made up of (#'s)
 // for each denoting level, each line denotes 1 less (#) and adds one space
 
+// we use a for loop instead of forEach because we are working with an integer and not an array
+
 function staircase(n) {
   for (let i = 1; i <= n; i++) {
     console.log(' '.repeat(n - i) + '#'.repeat(i));
@@ -9,3 +11,21 @@ function staircase(n) {
 }
 
 staircase(6);
+
+// second solution
+// build array and replace one character at a time
+
+// const staircase = (n) => {
+//   let arr = [];
+
+//   for (let x = 0; x < n; x++) {
+//     arr.push(' ');
+//   }
+
+//   for (x = n; x >= 0; x--) {
+//     arr[x] = '#';
+//     console.log(arr.join(''));
+//   }
+// };
+
+// staircase(10);
