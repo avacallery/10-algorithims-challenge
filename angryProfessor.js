e89e43b9391c359467ec1c645b8475ebd7396435
avacallery <avacallery@gmail.com>
2020-07-16 12:03:46 -0400

    angryProfessor algorithm

---
+++ angryProfessor.js
@@ -0,0 +1,15 @@
+// angryProfessor function must return 'YES' if class is cancelled, or 'NO'
+// minStudents is the threshold number of students for class to go on
+// arrivalTimes is an array of integers representing arrival times (class size)
+
+function angryProfessor(minStudents, arrivalTimes) {
+  arrivalTimes = arrivalTimes.filter((time) => time <= 0);
+  //   if (arrivalTimes.length >= minStudents) {
+  //     return 'NO';
+  //   }
+  //     return 'YES';
+
+  return arrivalTimes.length >= minStudents ? 'NO' : 'YES';
+}
+
+console.log(angryProfessor(3, [-1, 3, 2, 0, 0]));
