6d5a9659ec1ba5211d486fffb0dab134d189e64a
avacallery <avacallery@gmail.com>
2020-08-04 10:19:06 -0400

    cloud jumping array

---
+++ cloudJump.js
@@ -0,0 +1,24 @@
+// Emma can jump on any cloud (0)
+// Clouds that are numbered (1) must be avoided.
+// For the clouds she can step on, their placement in the array must be equal to the number of the cloud plus 1 or 2
+// so she can only ever skip (1) cloud at a time?
+
+// objective: print the minimum number of jumps needed to win the game
+
+function jumpingOnClouds(clouds) {
+  // count number of moves it takes to go through array
+  // avoid all (1)'s
+  let totalMoves = 0;
+  console.log(clouds.indexOf());
+
+  for (let i = 0; i < clouds.length - 1; i++) {
+    if (clouds[i] === 0) {
+      totalMoves++;
+    }
+  }
+  return totalMoves;
+}
+
+console.log(jumpingOnClouds([0, 0, 0, 0, 1, 0]));
+
+// array index 0, 1, 2, 3, 4, 5
