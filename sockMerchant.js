9f4aabd3628fc055bd00b254d3df9d838ed5c4fa
avacallery <avacallery@gmail.com>
2020-08-10 11:35:50 -0400

    sockMerchant algorithm - find duplicates in array

---
+++ sockMerchant.js
@@ -0,0 +1,21 @@
+// Determine how many pairs of socks with matching colors there are
+// numberOfSocks: the number of socks in the pile
+// typeOfSock: the colors of each sock (by number)
+
+// Return the total number of matching pairs
+
+function sockMerchant(numberOfSocks, typeOfSock) {
+  let sockPairs = 0;
+  let sortedSocks = typeOfSock.slice().sort();
+
+  sortedSocks.filter((sock, index) => {
+    if (sortedSocks.indexOf(sock) === index - 1) {
+      console.log(sortedSocks);
+      sockPairs++;
+    }
+  });
+
+  return sockPairs;
+}
+
+console.log(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]));
