c6ddba13648bdf8f72f3d1f7fe53a0a5e1d2af16
avacallery <avacallery@gmail.com>
2020-08-11 11:02:41 -0400

    jumping on clouds algorithm - in progress

--- cloudJump.js
+++ cloudJump.js
@@ -1,21 +1,26 @@
 // Emma can jump on any cloud (0)
 // Clouds that are numbered (1) must be avoided.
-// For the clouds she can step on, their placement in the array must be equal to the number of the cloud plus 1 or 2
+// For the clouds she can step on, the index in the array must be equal to the number of the current cloud index plus 1 or 2
 // so she can only ever skip (1) cloud at a time?
 
 // objective: print the minimum number of jumps needed to win the game
 
 function jumpingOnClouds(clouds) {
-  // count number of moves it takes to go through array
-  // avoid all (1)'s
   let totalMoves = 0;
 
-  for (let i = 0; i < clouds.length - 1; i++) {
-    clouds[i + 2] ? (totalMoves++, i++) : (totalMoves++, (i += 2));
-  }
+  // for (let i = 0; i < clouds.length - 1; i++) {
+  //   clouds[i + 2] ? (totalMoves++, i++) : (totalMoves++, (i += 2));
+  // }
+
+  clouds.forEach((cloud, index) => {
+    if (cloud === 0 && clouds.indexOf() === clouds[index + 1]) {
+    }
+    console.log(totalMoves);
+    totalMoves++;
+  });
   return totalMoves;
 }
 
-console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]));
+jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]);
 
 // array index 0, 1, 2, 3, 4, 5
