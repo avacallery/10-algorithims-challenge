5b7d289888fa20689439c1f5d7cf36941369cd0d
avacallery <avacallery@gmail.com>
2020-08-18 11:10:37 -0400

    cloud jump algorithm

--- cloudJump.js
+++ cloudJump.js
@@ -5,29 +5,35 @@
 
 // objective: print the minimum number of jumps needed to win the game
 
+// function jumpingOnClouds(clouds) {
+//   let totalMoves = 0;
+
+//   for (let i = 0; i < clouds.length; i++) {
+//     // if (clouds[i] === 0 && clouds[i + 1] === 0) {
+//     //   totalMoves++;
+//     // }
+//     // if (clouds[i + 2] === 0 && clouds[i + 1] === 0) {
+//     //   totalMoves++;
+//     // }
+//   }
+
+//   return totalMoves;
+// }
+
 function jumpingOnClouds(clouds) {
   let totalMoves = 0;
 
-  for (let i = 0; i < clouds.length; i++) {
-    // if (clouds[i] === 0 && clouds[i + 1] === 0) {
-    //   totalMoves++;
-    // }
-
-    if (clouds[i + 2] === 0 && clouds[i + 1] === 0) {
-      totalMoves++;
-    }
-    // clouds[i + 2] ? (totalMoves++, i++) : (totalMoves++, (i += 2));
+  for (let i = 0; i < clouds.length - 1; i++) {
+    if (clouds[i] === 0) i++;
+    totalMoves++;
   }
 
-  // clouds.forEach((cloud, index) => {
-  //   if (cloud === 0 && clouds[index + 1]) {
-  //   }
   console.log(totalMoves);
   return totalMoves;
 }
 
-// jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]);
+jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]);
 jumpingOnClouds([0, 0, 1, 0, 0, 0, 0, 1, 0, 0]);
 
-// array index 0, 1, 2, 3, 4, 5
+// 0, 0, 1, 0, 0, 1, 0 --> expected 4 moves
 // 10, 0 0 1 0 0 0 0 1 0 0 --> expected 6 moves
