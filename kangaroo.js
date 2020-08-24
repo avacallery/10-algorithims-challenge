a5136e69421faba9d9966982b5423f3cb7bbc71d
avacallery <avacallery@gmail.com>
2020-08-24 10:25:00 -0400

    kangaroo algebra

--- kangaroo.js
+++ kangaroo.js
@@ -8,6 +8,13 @@
 // // If x2 > x1 and v2 > v1 automatic NO
 // Find common denominator - multiples of v1 and v2 to make x1 === x2
 
+// x1 + v2 * N = x2 + v2 * N
+// x1 - x2 + v1 * N = v2 * N
+// x1 - x2 = v2 * N - v1 * N
+// x1 - x2 = N ( v2 - v1 )
+// x1 - x2 / v2 - v1 = N
+// If positive and if int, then kangaroos meet up
+
 function kangaroo(x1, v1, x2, v2) {
   let currentLocation1 = 0;
   let currentLocation2 = 0;
